772ce3dc366f8d18f85cb63d0a40728d6c84acb7
Justin Johnson <justin@rowajo.com>
2022-02-16 12:03:39 -0700

    Episode 11 - Complete

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post
+{
+    public static function find($slug)
+    {
+        if (! file_exists($path = resource_path("/posts/{$slug}.html"))) {
+            throw new ModelNotFOundException();
+        }
+
+        return cache()->remember("posts.{$slug}", 60*60, fn() => file_get_contents($path));
+    }
+    public static function all()
+    {
+        $files = File::files(resource_path("/posts"));
+
+        return array_map(fn($file) => $file->getContents(), $files);
+    }
+
+}
