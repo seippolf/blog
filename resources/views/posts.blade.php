3d4c2354c9b8ac059024b9813b7bf48793b2323b
Justin Johnson <justin@rowajo.com>
2022-02-21 11:27:38 -0700

    Episode 15 - Finished

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,14 @@
-@extends ('layout')
-
-@section ('content')
+<x-layout>
     @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <div>
-                {{ $post->excerpt }}
-            </div>
-        </article>
+    <article>
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
     @endforeach
-@endsection
+</x-layout>
