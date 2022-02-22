01dc9c40741ad9c60138a1f05f8df8274a35f4ee
Justin Johnson <justin@rowajo.com>
2022-02-22 14:05:39 -0700

    Episode 20 - Finished (Remember you are using Sail)

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::all()
     ]);
 });
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post}', function ($id) {
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => Post::findOrFail($id)
     ]);
 });
