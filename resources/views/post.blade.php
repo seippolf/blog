c6603e0bd5d9de3ee4ca1690c8c0aa07cea7b467
Justin Johnson <justin@rowajo.com>
2022-02-23 11:45:01 -0700

    Episode 24 - Complete

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,15 @@
 <x-layout> 
     <article>
         <h1>{!! $post->title !!}</h1>
+
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
+
         <div>
             {!! $post->body !!} 
         </div>
     </article>
+    
     <a href="/">Go Back</a>
 </x-layout>
