c6d918aa805e9f15dcdb32efbcc9f50e67a16885
Justin Johnson <justin@rowajo.com>
2022-03-10 14:21:59 -0700

    Episode 38 - Complete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,13 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters) 
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) => 
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orwhere('title', 'like', '%' . $search . '%'));
+    }
     public function category()
     {
         return $this->belongsTo(Category::class);
