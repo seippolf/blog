820070c491a592e3ef2d97c4f0ad7d10de6c86c9
Justin Johnson <justin@rowajo.com>
2022-06-22 16:07:24 -0700

    Episode 48 - Complete

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,6 +23,6 @@ class RegisterController extends Controller
             ])
         );
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -53,4 +53,7 @@
             </div>
         </footer>
     </section>
+
+    <x-flash/>
+
 </body>
