bec55f0af0a34efcbd68037ae1853df2ac338f34
Justin Johnson <justin@rowajo.com>
2022-06-22 14:17:33 -0700

    Episode 45 - Complete

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+        User::create(
+            request()->validate([
+                'name' => ['required', 'max:255'],
+                'username' => ['required', 'max:255', 'min:3'],
+                'email' => ['required', 'email', 'max:255'],
+                'password' => ['required', 'min:7', 'max:255']
+            ])
+        );
+
+        return redirect('/');
+    }
+}
